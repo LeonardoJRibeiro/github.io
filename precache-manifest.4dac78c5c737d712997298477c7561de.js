3120d54933acb8073d27d37f8c583a58cbe20bec
Leonardo Jardim Ribeiro <leonardojr410@gmail.com>
2020-08-23 17:20:22 -0300

    Updates

---
+++ precache-manifest.4dac78c5c737d712997298477c7561de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbbb205f1b54c5ba12def79c7884f3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8547135273f98e8cab5",
+    "url": "/static/css/main.57015fba.chunk.css"
+  },
+  {
+    "revision": "560387e9e86e57c6dfbb",
+    "url": "/static/js/2.35fe6c47.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.35fe6c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8547135273f98e8cab5",
+    "url": "/static/js/main.286dd64f.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  }
+]);
